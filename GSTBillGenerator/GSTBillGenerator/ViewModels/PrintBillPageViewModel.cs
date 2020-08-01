6c2319bf85aca9a67b91a39cea35218e00e38acc
Yash Thakur <yasht3210@gmail.com>
2020-08-01 14:15:24 +0530

    first commit

---
+++ GSTBillGenerator/GSTBillGenerator/ViewModels/PrintBillPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GSTBillGenerator.ViewModels
+{
+    public class PrintBillPageViewModel : ViewModelBase 
+    {
+        public PrintBillPageViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            Title = "Save as pdf";
+        }
+    }
+}
