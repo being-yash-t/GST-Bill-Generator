5c4b3137a89d872369af0953dd6ebefea41592ed
Yash Thakur <yasht3210@gmail.com>
2020-08-23 10:01:22 +0530

    Backup

---
+++ GSTBillGenerator/GSTBillGenerator/Pages/AddEditFirmPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using GSTBillGenerator.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GstBillGenerator.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddEditFirmPage : ContentPage
+    {
+        public AddEditFirmPage(FirmInfo existingItem)
+        {
+            InitializeComponent();
+        }
+    }
+}
--- GSTBillGenerator/GSTBillGenerator/Pages/FirmDetailsList.xaml.cs
+++ GSTBillGenerator/GSTBillGenerator/Pages/FirmDetailsList.xaml.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +10,11 @@ namespace GstBillGenerator.Pages
         public FirmDetailsList()
         {
             InitializeComponent();
+        }
+
+        private void CreateClicked(object sender, EventArgs e)
+        {
+            Shell.Current.Navigation.PushAsync(new AddEditFirmPage(null));
         }
     }
 }
